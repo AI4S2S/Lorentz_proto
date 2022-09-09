3bded3e9ada8f67671c523be96e8da577dc27526
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-09-09 10:18:52 +0200

    fix comments and print statement in save module

--- RGCPD/save.py
+++ RGCPD/save.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Functions to save forecast outputs i.e. scores and ML models
-https://medium.com/analytics-vidhya/save-and-load-your-scikit-learn-models-in-a-minute-21c91a961e9b
 """
 
-import numpy as np
 import pickle
 
 # save scores
@@ -16,5 +14,5 @@ def save_scores(df_file_name, df):
 # save ML model
 def save_model(pkl_model_filename, sklearn_model):
     pickle.dump(sklearn_model, open(pkl_model_filename, 'wb'))
-    print(f"Model is saved in onxx format in {pkl_model_filename}")
+    print(f"Model is saved in {pkl_model_filename}")
 
